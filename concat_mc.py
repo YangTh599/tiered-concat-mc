8741c5c2ff9a78bb71aef4be6b23311881c8a8a1
YangTh599 <YangTh599@northwested.org>
2024-09-20 10:19:33 -0400

    Create concat_mc.py

---
+++ concat_mc.py
@@ -0,0 +1,22 @@
+# Thayer Yang
+# 20 SEP 2024
+# Concatenation Check
+
+# Task 1
+first_name = 'Thayer'
+favorite_game = 'Valorant'
+
+print(first_name+" likes to play "+favorite_game)
+
+print()
+# Task 2
+
+print('What I\'ve learned about concatenation:' )
+print('- To convert numeric variables to be used in a string. It must be casted or converted by using the str() method.')
+print('- To concatenate variables you must end the quote, use a + operator, then use the variable. \n ex. : print(\'My name is \'+ first_name)')
+
+print()
+# Task 3
+fries_price = 2.29
+
+print('At Burger King, a large fries cost $'+str(fries_price))
